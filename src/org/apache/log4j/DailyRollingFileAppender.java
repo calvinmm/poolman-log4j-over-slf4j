4cfa3847f813ad53a780f97913cacc4ae0cdfd3f
Calvin MacKenzie <calvinm.mackenzie@gmail.com>
2023-01-03 13:34:24 -0800

    Add minimal implementation of DailyRollingFileAppender

---
+++ src/org/apache/log4j/DailyRollingFileAppender.java
@@ -0,0 +1,16 @@
+package org.apache.log4j;
+
+import java.io.IOException;
+
+/** Minimal implementation of the log4j DailyRollingFileAppender. */
+public class DailyRollingFileAppender {
+
+  public DailyRollingFileAppender() {
+    super();
+  }
+
+  public DailyRollingFileAppender(Layout layout, String filename, String datePattern)
+      throws IOException {
+    super();
+  }
+}
